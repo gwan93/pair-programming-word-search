58de51c442a1d4e0f7898536afe54aef1250bd37
vagrant <gio_wan@live.ca>
2021-02-10 23:22:46 +0000

    solution to pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// Pair Programming between Matthew Cook and Gio Wan
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
     }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const transposedArray = transpose(letters);
+    const transposedJoin = transposedArray.map(ls => ls.join(''));
+    // console.log(horizontalJoin);
+    for (l of horizontalJoin) {
+        if (l.includes(word)){return true}
+    } 
+    for (l of transposedJoin) {
+        if (l.includes(word)){return true}
+    }
+    return false;
 }
-
+const transpose = function(matrix) {
+    const outputMatrix = [];
+    let array = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        array.push(matrix[j][i]);
+      }
+      outputMatrix.push(array);
+      array = [];
+    }
+    return outputMatrix;
+};
 module.exports = wordSearch
